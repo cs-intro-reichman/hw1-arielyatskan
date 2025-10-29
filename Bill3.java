163066362986248af82543b9e0d2c7bf667729be
Ariel Yatskan <ariel.yatskan@post.runi.ac.il>
2025-10-29 11:31:22 +0200

    Fixed?

--- Bill3.java
+++ Bill3.java
@@ -1,4 +1,4 @@
-// Splits a restaurant bill evenly among three diners.
+// Splits a restaurant bill evenly among three diners..
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
