973603511a7699800a6c3da23c61e5d0d366e05d
Ariel Yatskan <ariel.yatskan@post.runi.ac.il>
2025-10-30 19:30:14 +0200

    Finnished and working

--- Bill3.java
+++ Bill3.java
@@ -7,9 +7,8 @@ public class Bill3 {
 		String name3 = args[2];
 		int bill = Integer.parseInt(args[3]); // Taking the forth String, which is the bill, 
 		                                      // and convert it to int form of Data
-		double share = bill / 3;
-		share = Math.ceil(share);
-		System.out.println("Dear " + name3 + ", " + name2 + " and " + name1 + 
-	                       ": Pay " + share + " Shekels each.");
+		double share = Math.ceil(bill / 3.0);
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + 
+	                       ": pay " + share + " Shekels each.");
 	}
 }
