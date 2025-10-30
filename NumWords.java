cf1e9a77eacf39169cad45f1600844a3f9f8fa5c
Ariel Yatskan <ariel.yatskan@post.runi.ac.il>
2025-10-30 12:45:40 +0200

    Finnished and working

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,36 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int num = Integer.parseInt(args[0]);
+		int ones = num % 10;
+		int tens = (num / 10) % 10;
+		int hundreds = (num / 100) % 10;
+		if(hundreds == 0) {
+			System.out.print("No hundreds, ");
+		} else {
+			if(hundreds == 1) {
+				System.out.print("1 hundred, ");
+			} else{
+				System.out.print(hundreds + " hundreds, ");
+			}
+		}
+		if(tens == 0) {
+			System.out.print("No tens, and ");
+		} else {
+			if(tens == 1) {
+				System.out.print("1 ten, and ");
+			} else{
+				System.out.print(tens + " tens, and ");
+			}
+		}
+		if(ones == 0) {
+			System.out.print("No ones.");
+		} else {
+			if(ones == 1) {
+				System.out.print("1 one.");
+			} else{
+				System.out.print(ones + " ones.");
+			}
+		}
 	}
 }
